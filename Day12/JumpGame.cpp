f24ecf62623fe6cebfa654837e4b091c99e2b5d1
ItsUtkarshhh <110464977+ItsUtkarshhh@users.noreply.github.com>
2024-07-27 22:47:23 +0530

    Create JumpGame.cpp

---
+++ Day12/JumpGame.cpp
@@ -0,0 +1,14 @@
+// Leetcode question number 55:
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = std::max(maxReach, i + nums[i]);
+        }
+    return true;
+    }
+};
