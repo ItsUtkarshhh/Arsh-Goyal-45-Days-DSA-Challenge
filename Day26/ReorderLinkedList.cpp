068f380989be094ad71a7276ae736df72181d4e2
ItsUtkarshhh <110464977+ItsUtkarshhh@users.noreply.github.com>
2024-08-10 23:23:07 +0530

    Create ReorderLinkedList.cpp

---
+++ Day26/ReorderLinkedList.cpp
@@ -0,0 +1,63 @@
+// Leetcode question number 143 :
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+    while (curr) {
+        ListNode* nextTemp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
+
+void reorderList(ListNode* head) {
+    if (!head || !head->next) return;
+
+    // Step 1: Find the middle of the list
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // Step 2: Reverse the second half of the list
+    ListNode* second = reverseList(slow->next);
+    slow->next = nullptr;  // Split the list into two halves
+
+    // Step 3: Merge the two halves
+    ListNode* first = head;
+    while (second) {
+        ListNode* temp1 = first->next;
+        ListNode* temp2 = second->next;
+
+        first->next = second;
+        second->next = temp1;
+
+        first = temp1;
+        second = temp2;
+    }
+}
+
+// Helper function to print the list
+void printList(ListNode* head) {
+    while (head) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+};
